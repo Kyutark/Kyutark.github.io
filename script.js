b4c305e8e2d02b0f45ddc4dfc21cc721e234a36e
Kyutark <88725013+Kyutark@users.noreply.github.com>
2023-10-27 19:55:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ fetch('data.json')
     .then(response => response.json())
     .then(json => {
         data = json;
+        // 데이터 로딩이 완료되면 검색 버튼 활성화
+        document.getElementById('searchButton').disabled = false;
     })
     .catch(error => {
         console.error("Error loading JSON data:", error);
