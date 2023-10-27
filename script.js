70ee6a05a88e3a2246f6e698d12891e3824baae2
Kyutark <88725013+Kyutark@users.noreply.github.com>
2023-10-27 19:16:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let data = {};
+
+// JSON 데이터 로드
+fetch('data.json')
+    .then(response => response.json())
+    .then(json => {
+        data = json;
+    })
+    .catch(error => {
+        console.error("Error loading JSON data:", error);
+    });
+
 function searchKeyword() {
     const searchInput = document.getElementById('searchInput').value;
     const result = data[searchInput];
